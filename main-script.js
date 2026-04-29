5f56bc9f107b102ada8a5423236d7ef9c56ccd46
Coleorion Coder <127061714+coleorioncoder@users.noreply.github.com>
2026-04-29 16:13:54 -0600

    Update main-script.js
    moved matrix math stuff from utils.js over to here, hopefully fixing canvas not rendering

--- main-script.js
+++ main-script.js
@@ -1,3 +1,24 @@
+var Mat4 = glMatrix.mat4;
+var M4 = {
+    identity: [
+        1, 0, 0, 0,
+        0, 1, 0, 0,
+        0, 0, 1, 0,
+        0, 0, 0, 1
+    ],
+    perspective: function(fieldOfViewInRadians, aspect, near, far) {
+        var f = Math.tan(Math.PI * 0.5 - 0.5 * fieldOfViewInRadians);
+        var rangeInv = 1.0 / (near - far);
+     
+        return [
+          f / aspect, 0, 0, 0,
+          0, f, 0, 0,
+          0, 0, (near + far) * rangeInv, -1,
+          0, 0, near * far * rangeInv * 2, 0
+        ];
+    },
+};
+
 var canvas1 = document.getElementById("main-canvas");
 
 var DOMvss = document.getElementById("vertex-vsh").textContent;
