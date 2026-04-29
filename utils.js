05fcecaf9dccca9b4b0097baf5a35e464a20c83b
Coleorion Coder <127061714+coleorioncoder@users.noreply.github.com>
2026-04-29 15:47:53 -0600

    Update utils.js
    Added some matrix math utils and glMatrix Mat4 variable

--- utils.js
+++ utils.js
@@ -1,3 +1,24 @@
+var Mat4 = glMatrix.mat4;
+var M4 = {
+    identity: [
+        1, 0, 0, 0,
+        0, 1, 0, 0,
+        0, 0, 1, 0,
+        0, 0, 0, 1
+    ],
+    perspective: function(fieldOfViewInRadians, aspect, near, far) {
+        var f = Math.tan(Math.PI * 0.5 - 0.5 * fieldOfViewInRadians);
+        var rangeInv = 1.0 / (near - far);
+     
+        return [
+          f / aspect, 0, 0, 0,
+          0, f, 0, 0,
+          0, 0, (near + far) * rangeInv, -1,
+          0, 0, near * far * rangeInv * 2, 0
+        ];
+    },
+};
+
 if (PVector == null) {
   function PVector(x, y, z) {
       this.x = x || 0;
