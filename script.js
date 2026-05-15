361ac0e8390787868989ed9857eff508a330f7cf
Coleorion Coder <127061714+coleorioncoder@users.noreply.github.com>
2026-05-15 14:42:07 -0600

    hopefully now fixed navbar functionality

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 var menus = document.getElementsByClassName("main-nav");
 var menuBtns = document.getElementsByClassName("nav-btn");
 
-menuBtns.forEach(btn => btn.addEventListener("click", (e) => {
-    menus.forEach(menu => {
-        if (menu.style.display == "none") {
-            menu.style.display = "block";
-        } else {
-            menu.style.display = "none";
+for (let i = 0; i < menuBtns.length; i++) {
+    let menuBtn = menuBtns[i];
+    menuBtn.addEventListener("click", function (e) {
+        for (let j = 0; j < menus.length; j++) {
+            let menu = menus[j];
+            if (menu.style.display == "none") {
+                menu.style.display = "block";
+            } else {
+                menu.style.display = "none";
+            }
         }
     });
-}));
+}
