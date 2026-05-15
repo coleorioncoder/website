acf99ecfc0592c2d4584124c870a4affcf3eda56
Coleorion Coder <127061714+coleorioncoder@users.noreply.github.com>
2026-05-15 15:05:23 -0600

    removed some debugging stuff

--- script.js
+++ script.js
@@ -8,14 +8,14 @@ for (let i = 0; i < menuBtns.length; i++) {
     menuBtn.addEventListener("click", function (e) {
         for (let j = 0; j < menus.length; j++) {
             let menu = menus[j];
-            document.body.innerHTML+="<span class=\"debug-text\">clicked</span>"
-            document.body.innerHTML+="<span class=\"debug-text\">"+menu.style.display+"</span>";
-            if (getComputedStyle(menu).display == "none") {
+            //document.body.innerHTML+="<span class=\"debug-text\">clicked</span>"
+            //document.body.innerHTML+="<span class=\"debug-text\">"+menu.style.display+"</span>";
+            if (window.getComputedStyle(menu).display == "none") {
                 menu.style.display = "block";
-                document.body.innerHTML+="<span class=\"debug-text\">displaying</span>"
+                //document.body.innerHTML+="<span class=\"debug-text\">displaying</span>"
             } else {
                 menu.style.display = "none";
-                document.body.innerHTML+="<span class=\"debug-text\">hiding</span>"
+                //document.body.innerHTML+="<span class=\"debug-text\">hiding</span>"
             }
         }
     });
